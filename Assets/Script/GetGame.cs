51304bd8a1c5ea9e39c491f7f8432738a90cc316
Júlia D. Craide <juliadcraide@hotmail.com>
2025-05-04 14:37:48 -0300

    Refactor Get Request to own file

--- Assets/Script/GetGame.cs
+++ Assets/Script/GetGame.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -8,7 +7,7 @@ using UnityEngine.Networking;
 // And https://stackoverflow.com/questions/36239705/serialize-and-deserialize-json-and-json-array-in-unity
 
 [Serializable]
-public class Game {
+public class InitGame {
     public string uuid;
     public int latestEp;
     public string seed;
@@ -16,9 +15,8 @@ public class Game {
 
 public class GetGame : MonoBehaviour {
     // Where to send our request
-    const string DEFAULT_URL = "http://localhost:3000/api/game";
     string seed;// = "eyJsYXRlc3RfZXAiOjE3MzYsInN0YXJ0X3RpbWVzdGFtcCI6MTc0NjMxODA0MTQzM30=";
-    string targetUrl = DEFAULT_URL;
+    string targetUrl = Request.DEFAULT_URL;
 
     // Keep track of what we got back
     string recentData = "";
@@ -27,31 +25,14 @@ public class GetGame : MonoBehaviour {
         if (!String.IsNullOrEmpty(this.seed)) {
             targetUrl += $"?seed={this.seed}";
         }
-        this.StartCoroutine(this.RequestRoutine(this.targetUrl, this.ResponseCallback));
-    }
-
-    // Web requests are typially done asynchronously, so Unity's web request system
-    // returns a yield instruction while it waits for the response.
-    //
-    private IEnumerator RequestRoutine(string url, Action<string> callback = null) {
-        // Using the static constructor
-        var request = UnityWebRequest.Get(url);
-
-        // Wait for the response and then get our data
-        yield return request.SendWebRequest();
-        var data = request.downloadHandler.text;
-
-        // This isn't required, but I prefer to pass in a callback so that I can
-        // act on the response data outside of this function
-        if (callback != null)
-            callback(data);
+        this.StartCoroutine(Request.GetRequestRoutine(this.targetUrl, this.ResponseCallback));
     }
 
     // Callback to act on our response data
     private void ResponseCallback(string data) {
         Debug.Log(data);
         recentData = data;
-        Game g = JsonUtility.FromJson<Game>(data);
+        InitGame g = JsonUtility.FromJson<InitGame>(data);
         Debug.Log(g.uuid);
         Debug.Log(g.latestEp);
         Debug.Log(g.seed);
---
+++ Assets/Script/Request.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Request {
+    public const string DEFAULT_URL = "http://localhost:3000/api/game";
+
+    // Web requests are typially done asynchronously, so Unity's web request system
+    // returns a yield instruction while it waits for the response.
+    static public IEnumerator GetRequestRoutine(string url, Action<string> callback = null) {
+        // Using the static constructor
+        var request = UnityWebRequest.Get(url);
+
+        // Wait for the response and then get our data
+        yield return request.SendWebRequest();
+        string data = request.downloadHandler.text;
+        long status = request.responseCode;
+        //Debug.Log(status);
+
+        if(status / 100 != 2) {
+            // Error
+        }
+
+        // This isn't required, but I prefer to pass in a callback so that I can
+        // act on the response data outside of this function
+        else if (callback != null) {
+            callback(data);
+        }
+    }
+}
