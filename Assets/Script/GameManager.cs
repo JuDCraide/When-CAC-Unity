05df343a87fca153a4e987684bfabdcef8836710
Júlia D. Craide <juliadcraide@hotmail.com>
2025-05-24 17:09:35 -0300

    Fix select round

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,5 +3,4 @@ using UnityEngine;
 public class GameManager:MonoBehaviour {
     public static string seed = null;// = "eyJsYXRlc3RfZXAiOjE3MzYsInN0YXJ0X3RpbWVzdGFtcCI6MTc0NjMxODA0MTQzM30=";
     public static Game game = null;
-
 }
--- Assets/Script/ShowRounds.cs
+++ Assets/Script/ShowRounds.cs
@@ -5,30 +5,29 @@ using UnityEngine;
 public class ShowRounds : MonoBehaviour {
     public int round = 1;
     public ShowRound[] rounds;
-    ShowRound currentRound = null;
-    public bool clicable = true;
+    public ShowRound currentRound = null;
+    public bool clicable = false;
 
     // Start is called before the first frame update
     void Start() {
-        currentRound = rounds[round - 1];
-        currentRound.setSelected(true);
+        currentRound = rounds[0];
+        SetRound(round);
     }
 
     // Update is called once per frame
     void Update() {
         if (!clicable && GameManager.game != null && GameManager.game.round != round) {
+            Debug.Log("Update: " + round + " " + GameManager.game.round);
             round = GameManager.game.round;
             SetRound(round);
         }
     }
 
     public void SetRound(int newRound) {
-        if (newRound != currentRound.number) {
-            this.round = newRound;
-            currentRound.setSelected(false);
-            currentRound = rounds[round - 1];
-            currentRound.setSelected(true);
-        }
+        this.round = newRound;
+        currentRound.setSelected(false);
+        currentRound = rounds[round - 1];
+        currentRound.setSelected(true);
     }
 
     public void SetRoundByClick(ShowRound showRound) {
